caf135c0f5d6ba4963ecbb10311ffd90739912a7
Wayfarer <petergeinitz@freenet.de>
2023-12-12 11:40:55 +0100

    Commit a working copy

--- Voxels/VoxelEngine.cs
+++ Voxels/VoxelEngine.cs
@@ -1,8 +1,8 @@
 ﻿// ReSharper disable PossibleLossOfFraction
 
 using System;
-using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Windows.Input;
 using Imaging;
@@ -12,30 +12,9 @@ namespace VoxelEngine
     /// <summary>
     ///     https://www.youtube.com/watch?v=bQBY9BM9g_Y
     /// </summary>
-    public class VoxelEngine
+    public sealed class VoxelEngine
     {
         private readonly Camera _camera = new();
-
-        /// <summary>
-        /// The y buffer
-        /// </summary>
-        private float[] _yBuffer;
-
-        private int _width = 1024;
-
-        private int _height = 1024;
-
-        /// <summary>
-        ///     The color map
-        ///     Buffer/array to hold color values (1024*1024)
-        /// </summary>
-        private Color[,] _colorMap;
-
-        /// <summary>
-        ///     The height map
-        ///     Buffer/array to hold height values (1024*1024)
-        /// </summary>
-        private int[,] _heightMap;
 
         /// <summary>
         ///     The scale factor
@@ -46,9 +25,34 @@ namespace VoxelEngine
 
         private readonly int _screenWidth = 772;
 
-        private DirectBitmap _dbm;
+        private int _distance = 300;
 
         private Bitmap _bmp;
+
+        private int _colorHeight;
+
+        /// <summary>
+        ///     The color map
+        ///     Buffer/array to hold color values (1024*1024)
+        /// </summary>
+        private Color[,] _colorMap;
+
+        private int _colorWidth;
+
+        /// <summary>
+        ///     The height map
+        ///     Buffer/array to hold height values (1024*1024)
+        /// </summary>
+        private int[,] _heightMap;
+
+        private int _topographyHeight;
+
+        private int _topographyWidth;
+
+        /// <summary>
+        ///     The y buffer
+        /// </summary>
+        private float[] _yBuffer;
 
         public VoxelEngine()
         {
@@ -67,52 +71,40 @@ namespace VoxelEngine
 
             for (var i = 0; i < _yBuffer.Length; i++) _yBuffer[i] = _screenHeight;
 
-            var sinPhi = Math.Sin(Math.PI * degrees / 180);
-            var cosphi = Math.Cos(Math.PI * degrees / 180);
+
+            var sinPhi = Math.Sin(Math.PI * degrees / 180d);
+            var cosPhi = Math.Cos(Math.PI * degrees / 180);
 
             float z = 1;
             float dz = 1;
+
             while (z < distance)
             {
                 var pLeft = new PointF(
-                    (float)(-cosphi * z - sinPhi * z) + point.X,
-                    (float)(sinPhi * z - cosphi * z) + point.Y);
+                    (float)(-cosPhi * z - sinPhi * z) + point.X,
+                    (float)(sinPhi * z - cosPhi * z) + point.Y);
                 var pRight = new PointF(
-                    (float)(cosphi * z - sinPhi * z) + point.X,
-                    (float)(-sinPhi * z - cosphi * z) + point.Y);
+                    (float)(cosPhi * z - sinPhi * z) + point.X,
+                    (float)(-sinPhi * z - cosPhi * z) + point.Y);
 
                 var dx = (pRight.X - pLeft.X) / _screenWidth;
                 var dy = (pRight.Y - pLeft.Y) / _screenWidth;
 
-                _dbm = DirectBitmap.GetInstance(_bmp);
-
                 for (var i = 0; i < _screenWidth; i++)
                 {
-                    var diffuseX = (int)pLeft.X & (_width - 1);
-                    var diffuseY = (int)pLeft.Y & (_height - 1);
-                    var heightX = (int)pLeft.X & (_width - 1);
-                    var heightY = (int)pLeft.Y & (_height - 1);
+                    var diffuseX = (int)pLeft.X & (_colorWidth - 1);
+                    var diffuseY = (int)pLeft.Y & (_colorHeight - 1);
+                    var heightX = (int)pLeft.X & (_topographyWidth - 1);
+                    var heightY = (int)pLeft.Y & (_topographyHeight - 1);
 
-                    //catch out of bound exception
-                    float heightOfHeightMap = _heightMap[heightX & (_heightMap.GetLength(1) - 1), heightY & (_heightMap.GetLength(0) - 1)];
+                    float heightOfHeightMap = _heightMap[heightX & (_topographyWidth - 1), heightY & (_topographyHeight - 1)];
 
                     var heightOnScreen = (height - heightOfHeightMap) / z * scaleHeight + horizon;
+                    var color = _colorMap[diffuseX & (_colorWidth - 1), diffuseY & (_colorHeight - 1)];
 
-                    //catch out of bound exception
-                    var color = _colorMap[diffuseX & (_colorMap.GetLength(1) - 1),
-                        diffuseY & (_colorMap.GetLength(0) - 1)];
-
-                    //TODO error hre
-                    var length = (int)(heightOnScreen - _yBuffer[i]);
-
-                    //DrawVerticalLine(i, (int)heightOnScreen, _yBuffer[i], color, g);
-                    if (heightOnScreen > _yBuffer[i]) continue;
-
-                    _dbm.DrawVerticalLine(i, (int)_yBuffer[i], length, color);
-
+                    DrawVerticalLine(color, i, (int)heightOnScreen, _yBuffer[i], _bmp);
 
                     if (heightOnScreen < _yBuffer[i]) _yBuffer[i] = heightOnScreen;
-
                     pLeft.X += dx;
                     pLeft.Y += dy;
                 }
@@ -121,7 +113,18 @@ namespace VoxelEngine
                 dz += 0.005f;
             }
 
-            return _dbm.Bitmap;
+            //var bmp = _dbm.Bitmap;
+            _bmp.Save("Result.jpg", ImageFormat.Jpeg);
+
+            return _bmp;
+        }
+
+        private void DrawVerticalLine(Color col, int x, int heightOnScreen, float buffer, Image bmp)
+        {
+            if (heightOnScreen > buffer) return;
+
+            using var g = Graphics.FromImage(bmp);
+            g.DrawLine(new Pen(new SolidBrush(col)), x, heightOnScreen, x, buffer);
         }
 
         public void KeyInput()
@@ -148,7 +151,7 @@ namespace VoxelEngine
 
         private void ClearFrame()
         {
-            _bmp = new Bitmap(_screenWidth + 1, _screenHeight +1);
+            _bmp = new Bitmap(_screenWidth, _screenHeight);
 
             using var g = Graphics.FromImage(_bmp);
 
@@ -158,29 +161,38 @@ namespace VoxelEngine
             var b = new SolidBrush(color);
 
             g.FillRectangle(b, 0, 0, _screenWidth, _screenHeight);
+
+            _bmp.Save("Frame.jpg", ImageFormat.Jpeg);
         }
 
         private void ProcessHeightMap(Bitmap bmp)
         {
             if (bmp == null) return;
-            var heightMap = DirectBitmap.GetInstance(bmp);
+
+            _topographyHeight = bmp.Height;
+            _topographyWidth = bmp.Width;
+
+            var dbm = DirectBitmap.GetInstance(bmp);
 
             _heightMap = new int[bmp.Width, bmp.Height];
             for (var i = 0; i < bmp.Width; i++)
             for (var j = 0; j < bmp.Height; j++)
-                _heightMap[i, j] = heightMap.GetPixel(i, j).R;
+                _heightMap[i, j] = dbm.GetPixel(i, j).R;
         }
 
         private void ProcessColorMap(Bitmap bmp)
         {
             if (bmp == null) return;
-            var colorMap = DirectBitmap.GetInstance(bmp);
+
+            _colorHeight = bmp.Height;
+            _colorWidth = bmp.Width;
+
+            var dbm = DirectBitmap.GetInstance(bmp);
 
             _colorMap = new Color[bmp.Width, bmp.Height];
             for (var i = 0; i < bmp.Width; i++)
             for (var j = 0; j < bmp.Height; j++)
-                _colorMap[i, j] = colorMap.GetPixel(i, j);
+                _colorMap[i, j] = dbm.GetPixel(i, j);
         }
-
     }
 }
